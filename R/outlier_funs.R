f54c0b8731a45711f2e8d13980aec89cedfd5524
Simon Graf <simon.graf@statistik.ji.zh.ch>
2024-10-25 15:51:16 +0000

    finalised refactoring for outlier_funs.R

--- R/outlier_funs.R
+++ R/outlier_funs.R
@@ -60,6 +60,7 @@ double_mad <- function(x, zero_mad_action = NULL){
 #' https://eurekastatistics.com/using-the-median-absolute-deviation-to-find-outliers/
 #'
 #' @inheritParams double_mad
+#'
 #' @importFrom stats median
 #'
 #' @return numeric vector of length length(x)
@@ -69,6 +70,7 @@ double_mad <- function(x, zero_mad_action = NULL){
 #' x <- c(1, 2, 3, 3, 4, 4, 4, 5, 5.5, 6, 6, 6.5, 7, 7, 7.5, 8, 9, 12, 52, 90)
 #'
 #' double_mad_from_median(x)
+#'
 
 double_mad_from_median <- function(x, zero_mad_action = NULL){
 
@@ -92,13 +94,14 @@ double_mad_from_median <- function(x, zero_mad_action = NULL){
 
 
 
-#' Detect Outliers via Median Absolute Deviation from the Median for asymmetric distributions
+#' Detect outliers using MAD from the median for asymmetric distributions
 #'
-#' Outlier detection based on MAD for asymetric distributions. The function calculates the distance to the median for every value in the
-#' distribution relative to the left or right side MAD. It then compares the value to your threshold and labels the outliers.
+#' Outlier detection based on Median Absolute Deviation (MAD) for asymetric distributions. The function calculates the distance
+#' to the median for every value in the distribution relative to the left or right side MAD. It then compares the value to your
+#' threshold and labels the outliers.
 #'
 #' @inheritParams double_mad
-#' @param thres z-score threshold (defaults to 3.5).
+#' @param thres z-score threshold (defaults to 3.5)
 #'
 #' @return logical vector
 #' @export
@@ -107,6 +110,7 @@ double_mad_from_median <- function(x, zero_mad_action = NULL){
 #' x <- c(1, 2, 3, 3, 4, 4, 4, 5, 5.5, 6, 6, 6.5, 7, 7, 7.5, 8, 9, 12, 52, 90)
 #'
 #' is_outlier_double_mad(x)
+#'
 
 is_outlier_double_mad <- function(x, zero_mad_action = NULL, thres = 3.5){
 
@@ -116,97 +120,66 @@ is_outlier_double_mad <- function(x, zero_mad_action = NULL, thres = 3.5){
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#' Get boundaries beyond which a value is an outlier via Median Absolute Deviation from the Median for asymmetric distributions
+#' Get boundaries beyond which a value is an outlier via MAD from the median for asymmetric distributions and IQR
 #'
-#' @param value variable of interest
-#' @param thres z-score threshold (defaults to 3.5, which is a popular choice).
-#' @param percent defaults to TRUE. Values below zero are set to 0, values over 100 to 100.
+#' Outlier detection based on Median Absolute Deviation (MAD) for asymetric distributions and interquartile range. The function
+#' calculates the distance to the median for every value in the distribution relative to the left or right side MAD. It then
+#' compares the value to your threshold and labels the outliers.
 #'
-#' @return tibble with numeric range
+#' @inheritParams is_outlier_double_mad
+#' @param percent Indicator for the scale of the data. If function is run for percantage data, the lower limit will not be negative
+#' while the upper limit does not exceed 100 percent. Defaults to TRUE.
+#'
+#' @importFrom stats median
+#' @importFrom stats IQR
+#'
+#' @return data.frame with numeric range
 #' @export
+#'
+#' @examples
+#' x <- c(1, 2, 3, 3, 4, 4, 4, 5, 5.5, 6, 6, 6.5, 7, 7, 7.5, 8, 9, 12, 52, 90)
+#'
+#' outlier_range(x)
+#'
 
-outlier_range<- function(value, thres=3.5,percent=TRUE){
+outlier_range <- function(x, zero_mad_action = NULL, thres = 3.5, percent = TRUE){
 
-  # iqr_thres as argument to allow for more strict criteria for groups with high variance?
-  # if(IQR(value)>iqr_thres) thres <- 1
-
-
-  data <- tibble(median=median(value),
-         iqr=IQR(value),
-         lower=median(value)-plausi::DoubleMAD(value)[1]*thres,
-         upper=median(value)+plausi::DoubleMAD(value)[2]*thres
+  # create table
+  data <- data.frame(
+    median = stats::median(x),
+    iqr = stats::IQR(x),
+    lower = round(stats::median(x) - plausi::double_mad(x)[1] * thres, 2),
+    upper = round(stats::median(x) + plausi::double_mad(x)[1] * thres, 2)
   )
 
-if(percent==TRUE){
+  # limit bandwidth in case of percentage scale
+  if(percent == TRUE){
 
- data <-data %>%
-    mutate(lower=ifelse(lower<0,0,round(lower,1)),
-           upper=ifelse(upper>100,100,round(upper,1)))
+    data <- data |>
+      mutate(
+        lower = ifelse(lower < 0, 0, lower),
+        upper = ifelse(upper > 100, 100, upper)
+      )
+  }
+
+  # add label column
+  data["label"] <- paste0(data$lower, " - ", data$upper)
 
 }
 
-  data %>%
-    mutate(label=paste(lower,"-",upper))
-
-}
 
 
-
-
-
-# useful functions for outlier detection (combined)
-# http://www.questionflow.org/2017/12/26/combined-outlier-detection-with-dplyr-and-ruler/
-
-
-#' Z-score with MAD
+#' Detect outliers using Z-score with MAD for symmetric distributions
 #'
-#' Outlier detection based on MAD. Median Absolute Deviation is a robust normalization unit based on median as a population center. In order to use MAD “as a consistent estimator for the estimation of the standard deviation” one takes its value multiplied by a factor.
+#' Outlier detection based on Median Absolute Deviation (MAD) for symetric distributions. The function calculates the distance
+#' to the median for every value in the distribution relative to the MAD. It then compares the value to your threshold and
+#' labels the outliers.
 #'
-#' @param x variable of interest
-#' @param thres z-score threshold (defaults to 3, which is a popular choice).
+#'
+#' @inheritParams double_mad
+#' @param thres z-score threshold (defaults to 3)
 #' @param na.rm remove NAs, defaults to TRUE
+#'
 #' @importFrom stats median
 #' @importFrom stats mad
 #'
@@ -216,22 +189,24 @@ if(percent==TRUE){
 #' @examples
 #' x <- c(1, 2, 3, 3, 4, 4, 4, 5, 5.5, 6, 6, 6.5, 7, 7, 7.5, 8, 9, 12, 52, 90)
 #'
-#' isnt_out_mad(x)
+#' is_outtlier_single_mad(x)
 #'
 
-isnt_out_mad <- function(x, thres = 3, na.rm = TRUE) {
-  abs(x - stats::median(x, na.rm = na.rm)) <= thres * stats::mad(x, na.rm = na.rm)
+is_outtlier_single_mad <- function(x, thres = 3, na.rm = TRUE) {
+
+  abs(x - stats::median(x, na.rm)) >= thres * stats::mad(x, na.rm)
+
 }
 
 
-#' Z-score
+#' Detect outliers using classic Z-scores for symmetric distributions
 #'
-#' Z-score, also called a standard score, of an observation is broadly speaking a distance from the population center measured in number of normalization units. The default choice for center is sample mean and for normalization unit is standard deviation.
+#' Outlier detection based on Z-scores for symetric distributions. The function calculates the Z-score, i. e. the distance of a value
+#' from the mean in number of standard deviations.
 #'
-#' @param x variable of interest
-#' @param thres z-score threshold (defaults to 3, which is a popular choice).
-#' @param na.rm remove NAs, defaults to TRUE
-#' @importFrom stats quantile
+#'
+#' @inheritParams is_outtlier_single_mad
+#'
 #' @importFrom stats sd
 #'
 #' @return logical vector
@@ -240,55 +215,46 @@ isnt_out_mad <- function(x, thres = 3, na.rm = TRUE) {
 #' @examples
 #' x <- c(1, 2, 3, 3, 4, 4, 4, 5, 5.5, 6, 6, 6.5, 7, 7, 7.5, 8, 9, 12, 52, 90)
 #'
-#' isnt_out_z(x)
+#' is_outlier_z(x)
 #'
 
-isnt_out_z <- function(x, thres = 3, na.rm = TRUE) {
-  abs(x - mean(x, na.rm = na.rm)) <= thres * stats::sd(x, na.rm = na.rm)
+is_outlier_z <- function(x, thres = 3, na.rm = TRUE) {
+
+  abs(x - mean(x, na.rm)) >= thres * stats::sd(x, na.rm)
+
 }
 
 
-#' Tukey’s fences
+
+#' Detect outliers using turkey's fences
 #'
-#' Tukey’s fences is a technique used in box plots. The non-outlier range is defined with [Q1−k(Q3−Q1), Q3+k(Q3−Q1)], where Q1 and Q3 are the lower and upper quartiles respectively, k - some nonnegative constant (popular choice is 1.5).
+#' Outlier detection based on turkey's fences. Tukey’s fences is a technique used in box plots. The non-outlier range is defined as
+#' [Q1−k(Q3−Q1), Q3+k(Q3−Q1)], where Q1 and Q3 are the lower and upper quartiles respectively and k - some non-negative constant
+#' (popular choice is 1.5).
 #'
-#' @param x variable of interest
-#' @param k defaults to 1.5
-#' @param na.rm remove NAs, defaults to TRUE
+#'
+#' @inheritParams double_mad
+#' @param thres Turkey's fences threshold (defaults to 1.5). Amount of how many IQRs bellow and above the first and third quartiles are
+#' accapted as within the bandwidth.
+#'
 #' @importFrom stats quantile
 #'
 #' @return logical vector
+#'
 #' @export
 #'
 #' @examples
 #' x <- c(1, 2, 3, 3, 4, 4, 4, 5, 5.5, 6, 6, 6.5, 7, 7, 7.5, 8, 9, 12, 52, 90)
 #'
-#' isnt_out_turkey(x)
+#' is_outlier_turkey(x)
+#'
 
-isnt_out_turkey <- function(x, k = 1.5, na.rm = TRUE) {
-  quar <- stats::quantile(x, probs = c(0.25, 0.75), na.rm = na.rm)
+is_outlier_turkey <- function(x, k = 1.5, na.rm = TRUE) {
+
+  quar <- stats::quantile(x, probs = c(0.25, 0.75), na.rm)
+
   iqr <- diff(quar)
 
-  (quar[1] - k * iqr <= x) & (x <= quar[2] + k * iqr)
+  (quar[1] - k * iqr >= x) | (x >= quar[2] + k * iqr)
+
 }
-
-
-# maha_dist <- . %>% select_if(is.numeric) %>%
-#   mahalanobis(center = colMeans(.), cov = cov(.))
-#
-# isnt_out_maha <- function(tbl, isnt_out_f, ...) {
-#   tbl %>% maha_dist() %>% isnt_out_f(...)
-# }
-
-# isnt_out_funs_long <- funs(
-#   z_long = isnt_out_z,
-#   mad_long = isnt_out_mad,
-#   turkey_long = isnt_out_turkey
-# )
-#
-
-# isnt_out_funs_cross <- funs(
-#   z_cross = isnt_out_z,
-#   mad_cross = isnt_out_mad,
-#   turkey_cross = isnt_out_turkey
-# )
